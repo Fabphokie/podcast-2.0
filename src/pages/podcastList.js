b77492d25973c103c4fcd0cdf40c8ac3c056064a
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-02-22 16:05:18 +0200

    added carousel and updated some css

--- src/pages/podcastList.js
+++ src/pages/podcastList.js
@@ -1,14 +1,32 @@
-
 import Footer from '@/Components/Footer';
 import Header from '@/Components/Header';
 import PodcastList from '@/Components/PodcastList';
-
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import styles from '../pages/podcastList.module.css'
 const PodcastListPage = () => {
   return (
-    <div>
+    <div className={styles.list}>
       <Header />
-      <h1>WELCOME BACK TO Fab_Podcast!</h1>
-      
+      <div className={styles.carouselContainer} style={{ marginTop: '20px' }}>
+        <Carousel  showStatus={false} showArrows={false} infiniteLoop autoPlay stopOnHover={false} interval={1500} className={styles.customCarousel}>
+          <div  classNames={styles.carouselSlide}>
+            <h1 className={styles.carouselTitle}>Welcome</h1>
+             
+          </div>
+          <div>
+            <h1 className={styles.carouselTitle}>Back</h1>
+          </div>
+          <div>
+            <h1 className={styles.carouselTitle}>to</h1>
+          </div>
+          <div>
+            <h1 className={styles.carouselTitle}>Fab_Podcast!</h1>
+          </div>
+          
+          {/* Add more slides if needed */}
+        </Carousel>
+      </div>
       <PodcastList />
       <Footer/>
     </div>
